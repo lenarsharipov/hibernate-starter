dc641e812036490f6e6d2feb8b32db1d92b44e04
Lenar Sharipov <lenarsharipov@gmail.com>
2024-05-20 20:56:07 +0300

    initial commit

---
+++ src/main/java/com/lenarsharipov/HibernateRunner.java
@@ -0,0 +1,21 @@
+package com.lenarsharipov;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.SQLException;
+
+public class HibernateRunner {
+
+    public static void main(String[] args) throws SQLException {
+
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+            Session session = sessionFactory.openSession()) {
+            System.out.println("OK");
+        }
+    }
+}
