4958b2e0580b9fc469707f61c37916fb2e2b771f
Lenar Sharipov <lenarsharipov@gmail.com>
2024-05-20 21:59:36 +0300

    Entity lesson commit

--- src/main/java/com/lenarsharipov/HibernateRunner.java
+++ src/main/java/com/lenarsharipov/HibernateRunner.java
@@ -1,21 +1,47 @@
 package com.lenarsharipov;
 
+import com.lenarsharipov.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class HibernateRunner {
 
     public static void main(String[] args) throws SQLException {
 
         Configuration configuration = new Configuration();
+        /*
+        * Поле metadataSources отвечает за сущности
+        * (нашу метаинформацию)
+        * public Configuration addAnnotatedClass(Class annotatedClass) {
+        *       metadataSources.addAnnotatedClass(annotatedClass);
+        *       return this;
+        * }
+        * */
+//        configuration.addAnnotatedClass(User.class);
+        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
         configuration.configure();
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
             Session session = sessionFactory.openSession()) {
-            System.out.println("OK");
+
+            session.beginTransaction();
+
+            User user = User.builder()
+                    .username("ivan@gmail.com")
+                    .firstname("Ivan")
+                    .lastname("Ivanov")
+                    .birthDate(LocalDate.of(2000, 1, 19))
+                    .age(24)
+                    .build();
+
+            session.persist(user);
+
+            session.getTransaction().commit();
         }
     }
 }
---
+++ src/main/java/com/lenarsharipov/entity/User.java
@@ -0,0 +1,33 @@
+package com.lenarsharipov.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private String username;
+
+    private String firstname;
+
+    private String lastname;
+
+//    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    private Integer age;
+
+}
