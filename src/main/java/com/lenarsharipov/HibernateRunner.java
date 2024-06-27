0661c4c060d469738a34d873703d16d54b0d7383
Lenar Sharipov <lenarsharipov@gmail.com>
2024-06-27 12:21:29 +0300

    JPA Transaction lesson commit

--- src/main/java/com/lenarsharipov/HibernateRunner.java
+++ src/main/java/com/lenarsharipov/HibernateRunner.java
@@ -1,54 +1,51 @@
 package com.lenarsharipov;
 
 import com.lenarsharipov.entity.Company;
+import com.lenarsharipov.entity.Payment;
 import com.lenarsharipov.entity.PersonalInfo;
 import com.lenarsharipov.entity.User;
 import com.lenarsharipov.util.HibernateUtil;
 import com.lenarsharipov.util.TestDataImporter;
+import jakarta.transaction.Transactional;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.annotations.QueryHints;
 import org.hibernate.graph.RootGraph;
+import org.hibernate.jdbc.Work;
 import org.hibernate.jpa.AvailableHints;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
 public class HibernateRunner {
 
+    @Transactional
     public static void main(String[] args) {
         try (SessionFactory sf = HibernateUtil.buildSessionFactory();
             var session = sf.openSession()) {
-            session.beginTransaction();
-//            session.enableFetchProfile("withCompanyAndPayment");
+            session.doWork(new Work() {
+                @Override
+                public void execute(Connection connection) throws SQLException {
+                    System.out.println(connection.getTransactionIsolation());
 
-//            Map<String, Object> properties = Map.of(
-//                    AvailableHints.HINT_SPEC_FETCH_GRAPH, session.getEntityGraph("WithCompanyAndChat")
-//            );
-//            User user = session.find(User.class, 1L, properties);
-//            System.out.println(user.getCompany().getName());
-//            System.out.println(user.getUserChats().size());
-//            System.out.println(user.getPayments().size());
+                }
+            });
 
-//            List<User> users = session.createQuery(
-//                    "select u from User u ",
-//                                User.class)
-//                    .setHint(AvailableHints.HINT_SPEC_FETCH_GRAPH, session.getEntityGraph("WithCompanyAndChat"))
-//                    .list();
-//            users.forEach(user -> System.out.println(user.getUserChats()));
-//            users.forEach(user -> System.out.println(user.getCompany().getName()));
-//            users.forEach(user -> System.out.println(user.getPayments()));
-
-            List<User> users = session.createQuery(
-                            "select u from User u",
-                            User.class)
-                    .list();
-            users.forEach(user -> System.out.println(user.getUserChats()));
-            users.forEach(user -> System.out.println(user.getCompany().getName()));
-            users.forEach(user -> System.out.println(user.getPayments()));
-
-            session.getTransaction().commit();
+//            try {
+//                session.beginTransaction();
+//
+//                Payment payment1 = session.find(Payment.class, 1L);
+//                Payment payment2 = session.find(Payment.class, 2L);
+//
+//                session.getTransaction().commit();
+//            } catch (Exception exception) {
+//                session.getTransaction().rollback();
+//                throw exception;
+//            }
+//            session.persist();
         }
     }
 }
